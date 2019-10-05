817c948fc63fe560f79d4343243e88c66ffcfe03
J. Adolfo <49995666+adolfohermosillo@users.noreply.github.com>
2019-10-04 21:42:35 -0500

    event_storms

--- event_storms_query.sql
+++ event_storms_query.sql
@@ -3,15 +3,15 @@ CREATE TABLE storm_events_modeled.StormEvents AS
 SELECT *
 FROM 
 (SELECT 
-event_id, event_type, year, month_name, begin_day, state_fips, cz_fips, injuries_direct, injuries_indirect, deaths_direct, deaths_indirect
+event_id, event_type, year, month_name, begin_day, state_fips, cz_fips, injuries_direct, injuries_indirect, deaths_direct, deaths_indirect, damage_crops
 FROM storm_events_staging.StormEvents_2013
 UNION DISTINCT
-SELECT event_id, event_type, year, month_name, begin_day, state_fips, cz_fips, injuries_direct, injuries_indirect, deaths_direct, deaths_indirect
+SELECT event_id, event_type, year, month_name, begin_day, state_fips, cz_fips, injuries_direct, injuries_indirect, deaths_direct, deaths_indirect, damage_crops
 FROM storm_events_staging.StormEvents_2012
 UNION DISTINCT
-SELECT event_id, event_type, year, month_name, begin_day, state_fips, cz_fips, injuries_direct, injuries_indirect, deaths_direct, deaths_indirect
+SELECT event_id, event_type, year, month_name, begin_day, state_fips, cz_fips, injuries_direct, injuries_indirect, deaths_direct, deaths_indirect, damage_crops
 FROM storm_events_staging.StormEvents_2011
 UNION DISTINCT
-SELECT event_id, event_type, year, month_name, begin_day, state_fips, cz_fips, injuries_direct, injuries_indirect, deaths_direct, deaths_indirect
+SELECT event_id, event_type, year, month_name, begin_day, state_fips, cz_fips, injuries_direct, injuries_indirect, deaths_direct, deaths_indirect, damage_crops
 FROM storm_events_staging.StormEvents_2010)
-ORDER BY event_id, event_type, year, month_name, begin_day, state_fips, cz_fips, injuries_direct, injuries_indirect, deaths_direct, deaths_indirect
+ORDER BY event_id, event_type, year, month_name, begin_day, state_fips, cz_fips, injuries_direct, injuries_indirect, deaths_direct, deaths_indirect, damage_crops
