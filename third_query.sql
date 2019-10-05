fd26955996a112068ffb576be9f4eac8d1abe5a4
Basilio0505 <44238148+Basilio0505@users.noreply.github.com>
2019-10-04 22:22:30 -0500

    Update third_query.sql

--- third_query.sql
+++ third_query.sql
@@ -1,8 +1,10 @@
---Events joined to see tornados happened in different counties within states
+--Events joined to see tornados happened in different counties within states ordered chronologically
 SELECT
-year, month_name, cz_name, location
+a.year, a.month_name, cz_name, location
 FROM storm_events_modeled.StormEvents a
 FULL OUTER JOIN storm_events_modeled.Locations b
 ON a.cz_fips = b.cz_fips AND a.state_fips = b.state_fips
+JOIN storm_events_modeled.Date c
+ON a.year = c.year AND a.month_name = c.month_name AND a.begin_day = c.begin_day
 WHERE event_type = 'Tornado'
-ORDER BY year, month_name
+ORDER BY date
